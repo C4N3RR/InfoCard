c424b2e696f199b05499818b15354d4c8b7f4542
Caner Davarcı <candav0671@gmail.com>
2026-06-07 03:30:37 +0300

    Initial Commit

---
+++ InfoCard/InfoCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfoCardApp.swift
+//  InfoCard
+//
+//  Created by Caner Davarcı on 7.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct InfoCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InfoCardTests/InfoCardTests.swift
@@ -0,0 +1,17 @@
+//
+//  InfoCardTests.swift
+//  InfoCardTests
+//
+//  Created by Caner Davarcı on 7.06.2026.
+//
+
+import Testing
+@testable import InfoCard
+
+struct InfoCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
